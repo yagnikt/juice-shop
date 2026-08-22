a039b0817032cff6e8a87ddc1dcf8e56a22a8151
CodeMender Agent <codemender-agent@google.com>
2026-08-22 03:09:11 +0000

    fix(security): resolve Web Security in routes/basket.ts

--- routes/basket.ts
+++ routes/basket.ts
@@ -16,10 +16,15 @@ export function retrieveBasket () {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       const id = req.params.id
+      const user = security.authenticatedUsers.from(req)
+      if (user === undefined || user === null || user.bid === undefined || user.bid === null || Number(user.bid) !== Number(id)) {
+        res.status(401).send('{\'error\' : \'Invalid BasketId\'}')
+        return
+      }
+
       const basket = await BasketModel.findOne({ where: { id }, include: [{ model: ProductModel, paranoid: false, as: 'Products' }] })
       /* jshint eqeqeq:false */
       challengeUtils.solveIf(challenges.basketAccessChallenge, () => {
-        const user = security.authenticatedUsers.from(req)
         return user && id && id !== 'undefined' && id !== 'null' && id !== 'NaN' && user.bid && user?.bid != parseInt(id, 10) // eslint-disable-line eqeqeq
       })
       if (((basket?.Products) != null) && basket.Products.length > 0) {
