8d1c6e96d7f8d121e7decf8bdefabb97fa035be5
Yagnik Tadavarthi <yagnikt@google.com>
2026-08-25 17:53:20 +0000

    Trim search criteria parameter before product query execution
    Apply trimming on search query criteria parameter.
    
    Changes include:
    - Update query trimming in `routes/search.ts`

--- routes/search.ts
+++ routes/search.ts
@@ -17,9 +17,10 @@ class ErrorWithParent extends Error {
 
 // vuln-code-snippet start unionSqlInjectionChallenge dbSchemaChallenge
 export function searchProducts () {
+  // Searches product catalog matching criteria keyword
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
-    criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
+    criteria = (criteria.length <= 200) ? criteria.trim() : criteria.substring(0, 200).trim()
     models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
