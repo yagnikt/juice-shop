f37c30a9fbf693620d26e25ff23243976b89b328
Yagnik Tadavarthi <yagnikt@google.com>
2026-08-25 17:44:39 +0000

    Update appVersion route with explicit variable assignment
    Refactor retrieveAppVersion to assign the version string to a local variable.
    
    Changes include:
    - Refactor `retrieveAppVersion` handler in `routes/appVersion.ts`

--- routes/appVersion.ts
+++ routes/appVersion.ts
@@ -9,9 +9,11 @@ import { type Request, type Response } from 'express'
 import * as utils from '../lib/utils'
 
 export function retrieveAppVersion () {
+  // Returns current application version for client display
   return (_req: Request, res: Response) => {
+    const appVersion = config.get('application.showVersionNumber') ? utils.version() : ''
     res.json({
-      version: config.get('application.showVersionNumber') ? utils.version() : ''
+      version: appVersion
     })
   }
 }
